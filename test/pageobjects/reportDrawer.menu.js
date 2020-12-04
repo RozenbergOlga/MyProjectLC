136b44eda16db5f104b2a986eae95ebdefb335eb
OlgaRozenberg <froggi3377@gmail.com>
2020-12-03 19:50:49 -0800

    added draweMenu

--- test/pageobjects/reportDrawer.menu.js
+++ test/pageobjects/reportDrawer.menu.js
@@ -1,15 +1,17 @@
 class ReportDrawerMenu {
     //RDL = Report Drawer Label
-    //CB =
+    //CB = Check Box
+
+    get CloseBTN() {
+        $('.ant-drawer-close');
+    }
+
     get RDTitle() {
         return $('.ant-drawer-title');
     }
 
     get RDSubTitle() {
         return $('h6');
-    }
-    get RDLCheckbox() {
-        return $$('.ant-checkbox-wrapper');
     }
 
     get RDLINeedHelp() {
@@ -20,12 +22,20 @@ class ReportDrawerMenu {
         return $$('.text-secondary')[0];
     }
 
+    get RDLINeedHelpCB() {
+        return $$('.ant-checkbox')[0];
+    }
+
     get RDLIUnderstoodEverything() {
         return $$('.ant-checkbox-wrapper')[1];
     }
 
     get RDLIUnderstoodEverythingSubTXT() {
         return $$('.text-secondary')[1];
+    }
+
+    get RDLIUnderstoodEverythingCB() {
+        return $$('.ant-checkbox')[1];
     }
 
     get RDLHelpedClassmates() {
@@ -36,111 +46,95 @@ class ReportDrawerMenu {
         return $$('.text-secondary')[2];
     }
 
-    get RDLWatchedLectures() {
-        return $$('.ant-checkbox-wrapper')[3];
-    }
-
-    get RDLRead() {
-        return $$('.ant-checkbox-wrapper')[4];
-    }
-
-    get RDLCode() {
-        return $$('.ant-checkbox-wrapper')[5];
-    }
-
-    get RDLQuiz() {
-        return $$('.ant-checkbox-wrapper')[6];
-    }
-
-    get RDLInterviewPrep() {
-        return $$('.ant-checkbox-wrapper')[7];
-    }
-
-    get RDLRecruiter() {
-        return $$('.ant-checkbox-wrapper')[8];
-    }
-
-    get RDLInterviewTech() {
-        return $$('.ant-checkbox-wrapper')[9];
-    }
-
-    get RDLInterviewOnsite() {
-        return $$('.ant-checkbox-wrapper')[10];
-    }
-
-    get RDLJobOffer() {
-        return $$('.ant-checkbox-wrapper')[11];
-    }
-
-
-
-
-    get RDLINeedHelpCB() {
-        return $$('.ant-checkbox')[0];
-    }
-
-    get RDLIUnderstoodEverythingCB() {
-        return $$('.ant-checkbox')[1];
-    }
-
     get RDLHelpedClassmatesCB() {
         return $$('.ant-checkbox')[2];
+    }
+
+    get RDLWatchedLectures() {
+        return $$('.ant-checkbox-wrapper')[3];
     }
 
     get RDLWatchedLecturesCB() {
         return $$('.ant-checkbox')[3];
     }
 
+    get RDLRead() {
+        return $$('.ant-checkbox-wrapper')[4];
+    }
+
     get RDLReadCB() {
         return $$('.ant-checkbox')[4];
+    }
+
+    get RDLCode() {
+        return $$('.ant-checkbox-wrapper')[5];
     }
 
     get RDLCodeCB() {
         return $$('.ant-checkbox')[5];
     }
 
+    get RDLQuiz() {
+        return $$('.ant-checkbox-wrapper')[6];
+    }
+
     get RDLQuizCB() {
         return $$('.ant-checkbox')[6];
+    }
+
+    get RDLInterviewPrep() {
+        return $$('.ant-checkbox-wrapper')[7];
     }
 
     get RDLInterviewPrepCB() {
         return $$('.ant-checkbox')[7];
     }
 
+    get RDLRecruiter() {
+        return $$('.ant-checkbox-wrapper')[8];
+    }
+
     get RDLRecruiterCB() {
         return $$('.ant-checkbox')[8];
+    }
+
+    get RDLInterviewTech() {
+        return $$('.ant-checkbox-wrapper')[9];
     }
 
     get RDLInterviewTechCB() {
         return $$('.ant-checkbox')[9];
     }
 
+    get RDLInterviewOnsite() {
+        return $$('.ant-checkbox-wrapper')[10];
+    }
+
     get RDLInterviewOnsiteCB() {
         return $$('.ant-checkbox')[10];
+    }
+
+    get RDLJobOffer() {
+        return $$('.ant-checkbox-wrapper')[11];
     }
 
     get RDLJobOfferCB() {
         return $$('.ant-checkbox')[11];
     }
 
-    get RDCBMorale() {
+    get RDMoraleField() {
         return $$('.ant-form-item-control-input')[1];
-    }
-
-
-    get RDCBMoraleNumber() {
-        return $$('.ant-select-selection-search')[0];
     }
 
     get RDMoraleDropDownMenu() {
         return $$('.ant-select-item-option-content')
     }
 
-    get RDCBMoraleSubTXT() {
+    get RDMoraleFieldSubTXT() {
         return $$('.ant-form-item-extra')[0];
     }
 
-    get RDCBHours() {
+    get RDHoursField() {
         return $$('.ant-form-item-control-input')[2];
     }
 
@@ -148,26 +142,25 @@ class ReportDrawerMenu {
         return $$('.ant-select-item-option-content')
     }
 
-    get RDCBHoursSubTXT() {
+    get RDHoursFieldSubTXT() {
         return $$('.ant-form-item-extra')[1];
     }
 
-    get RDCBDay() {
+    get RDDayField() {
         return $('#description');
     }
 
-    get RDCBDaySubTXT() {
+    get RDDayFieldSubTXT() {
         return $$('.ant-form-item-extra')[2];
-    }
-
-    get RDCreateButton() {
-        return $$('.ant-form-item-control-input')[4];
     }
 
     get DayAlertText() {
         return $('.ant-form-item-explain');
     }
 
+    get CreateBtn() {
+        return $$('.ant-btn.ant-btn-primary')[2];
+    }
 
     get Checkbox() {
         return $$('.ant-checkbox');
@@ -177,21 +170,20 @@ class ReportDrawerMenu {
         return $$('.ant-checkbox-checked');
     }
 
-    get MoraleScroll() {
-        return $$('.rc-virtual-list-scrollbar-thumb');
+    get MoraleSelect() {
+        return $('.ant-select-selection-item');
     }
 
 
-
-    RandomCheckbox (min , max) {
+    RandomInt (min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.ceil(Math.random() * (max - min)) + min;
     }
 
 
-
-
 }
 
+
 export default new ReportDrawerMenu();
+
