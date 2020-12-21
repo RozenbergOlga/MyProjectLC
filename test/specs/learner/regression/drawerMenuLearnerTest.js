e59560dd2fef43e2febc55206449eb943c1db63d
OlgaRozenberg <froggi3377@gmail.com>
2020-12-20 20:44:29 -0800

    added drawerMenuLearnerTest.js

--- test/specs/learner/regression/drawerMenuLearnerTest.js
+++ test/specs/learner/regression/drawerMenuLearnerTest.js
@@ -330,7 +330,6 @@ describe('DRAWER MENU', () => {
     browser.pause(2000);
     expect(ReportDrawerMenu.DayAlertText.isDisplayed()).toEqual(true);
   });
-
   xit(
     'TC-44 Verify that you could not use copy past for inputing uncorrect numbers of characters ' +
       '(less then 30) in the field "How was your day?"',
@@ -340,23 +339,18 @@ describe('DRAWER MENU', () => {
       });
     },
   );
-
   it('TC-45.1 Verify that btn Create is presented on the page and title is correct', () => {
     expect(ReportDrawerMenu.CreateBtn.isDisplayed()).toEqual(true);
   });
-
   it('TC-45.2 Verify that btn Create is presented on the page and title is correct', () => {
     expect(ReportDrawerMenu.CreateBtn.getText()).toEqual(drawerMenu.createBtn);
   });
-
   it('TC-46 Verify that btn Create is clickable', () => {
     expect(ReportDrawerMenu.CreateBtn.isClickable()).toEqual(true);
   });
-
   it('TC-47 Verify that btn "X" is presented on the page', () => {
     expect(ReportDrawerMenu.XButton.isDisplayed()).toEqual(true);
   });
-
   it('TC-48 Verify that btn "X" is clickable', () => {
     expect(ReportDrawerMenu.XButton.isClickable()).toEqual(true);
   });
