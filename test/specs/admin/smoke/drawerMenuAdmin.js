0d057d31aa3d0be440c45ac4111e44f7eded657e
OlgaRozenberg <froggi3377@gmail.com>
2021-05-02 21:50:03 -0700

    drawerMenuAdmin.js

--- test/specs/admin/smoke/drawerMenuAdmin.js
+++ test/specs/admin/smoke/drawerMenuAdmin.js
@@ -1,8 +1,12 @@
 
 import LoginPage from '../../../pageobjects/login.page';
+
 import ProfilePage from '../../../pageobjects/profile.page';
+
 import user from '../../../../data/users.data';
+
 import { topMenu, profilePage, diaryPage, drawerMenu } from '../../../../data/expected.data';
+
 import ReportDrawerMenu from '../../../pageobjects/reportDrawer.menu';
 
 describe('REPORT DRAWER MENU', () => {
