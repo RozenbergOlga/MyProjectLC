ea0dead2e1ea59fdc6eea429986b61877ab3ef25
OlgaRozenberg <froggi3377@gmail.com>
2021-11-08 21:20:48 -0800

    profilePageAdmin.js

--- test/specs/admin/smoke/profilePageAdmin.js
+++ test/specs/admin/smoke/profilePageAdmin.js
@@ -4,7 +4,9 @@ import user from '../../../../data/users.data';
 import { topMenu, profilePage, diaryPage } from '../../../../data/expected.data';
 
 describe('VERIFY THAT PROFILE PAGE HAS DIARY ELEMENTS', () => {
+
   describe('VERIFY THAT PROFILE PAGE HAS DIARY ELEMENTS IN TOP MENU FOR ADMIN', () => {
+
     before(() => {
       LoginPage.open();
       browser.maximizeWindow();
